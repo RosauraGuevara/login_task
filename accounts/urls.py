367a31341a72c6ae911c39743a0fdb3680c63ea2
rosauraGuevara <rouscenturin@gmail.com>
2024-08-04 02:23:01 -0400

    actualización

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
@@ -15,7 +16,7 @@ urlpatterns = [
         redirect_authenticated_user=True,
         template_name='accounts/login.html'
     ), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('user_detail/<int:pk>/', views.UserDetail.as_view(), name='user_detail'),
     path('user_update/<int:pk>/', views.UserUpdate.as_view(), name='user_update'),
     path('password_change/', views.PasswordChange.as_view(), name='password_change'),
