6d990aa5dda6b94e294a4f351264424fecdf73b0
rosauraGuevara <rouscenturin@gmail.com>
2024-08-04 01:23:36 -0400

    actualización

--- loginTask/settings.py
+++ loginTask/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'blog.middleware.auth.AuthMiddleware',
+    'tasks.middleware.auth.AuthMiddleware',
 ]
 
 ROOT_URLCONF = 'loginTask.urls'
--- loginTask/urls.py
+++ loginTask/urls.py
@@ -3,8 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tasks/', include('tasks.urls')),
+    path('tasks/', include(('tasks.urls', 'tasks'), namespace='task')),
     path('accounts/', include('accounts.urls')),
 ]
-
-
